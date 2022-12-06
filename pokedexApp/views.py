043de9661431966441209cac3d4430405d96a505
Samave <samave02@hotmail.com>
2022-12-05 23:55:00 -0500

    correciones

--- pokedexApp/views.py
+++ pokedexApp/views.py
@@ -3,7 +3,7 @@ import urllib.request
 import json
 from http import HTTPStatus
 from urllib.error import HTTPError
-
+import requests
 # Create your views here.
 
 def inicio(request):
@@ -14,36 +14,25 @@ def index(request):
     try:
         if request.method == 'POST':
             pokemon = request.POST['pokemon'].lower()
-            pokemon = pokemon.replace(' ', '%20')
-            url_pokeapi = urllib.request.Request(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
-            url_pokeapi.add_header('User-Agent', "pikachu")
-            source = urllib.request.urlopen(url_pokeapi).read()
+            pokemon = pokemon.replace(' ','%20')
+            # Request de la api
+            url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}/'
+            res = requests.get(url).json()
+            # ----------------- variables de la api----------------------------------
+            id,nombre,altura,peso = res['id'],res['name'],str(res['height']/10)+' m',str(res['weight'])+" lbs"
+            tipo  = []
+            imagen = res['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+            for i in res['types']:
+                tipo.append(i['type']['name'])
+            for i in res['abilities']:
+                if i['is_hidden'] == True:
+                    habiliadadOculta = i['ability']['name']
+                else:
+                    habilidad = i['ability']['name']
 
-            # Convirtiendo el JSON a un diccionario
-            # 'list_of_data' guardará todos los datos que estamos solicitando
-            list_of_data = json.loads(source)
-                
-            # La variable 'data' guardará todo lo que vamos a renderizar en HTML
-            # Las llaves y valores las provee la API de Pokemon
-
-            # Altura de decímetros a metros
-            height_obtained = (float(list_of_data['height']) * 0.1)
-            height_rounded = round(height_obtained, 2)
-
-            # Peso de hectogramos a kilogramos
-            weight_obtained = (float(list_of_data['weight']) * 0.1)
-            weight_rounded = round(weight_obtained, 2)
-
-            data = {
-                "number": str(list_of_data['id']),
-                "name": str(list_of_data['name']).capitalize(),
-                "height": str(height_rounded)+ " m",
-                "weight": str(weight_rounded)+ " kg",
-                "sprite": str(list_of_data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']),
-            }
-
+            # -------------------base de datos---------------------------------------
+            data = {'id':id,'nombre':nombre,'tipo':tipo,'altura':altura,'peso':peso,'habilidad':habilidad,'habiliadadOculta':habiliadadOculta,'imagen':imagen}
             print(data)
-        
         else:
             data = {}
 
